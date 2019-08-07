e29ee7a7442fd3f087d8c31df11bab01cb7bc3d3
Martin Lucina <martin@lucina.net>
2019-08-07 15:55:23 +0200

    config.ml: Update, remove manual network configuration
    Update to newest conventions, remove unneeded ~packages.
    
    The network stack is now configured via command line options (--ipv4,
    --ipv4-gateway), so remove the manual network configuration.

--- iperf_client/config.ml
+++ iperf_client/config.ml
@@ -1,18 +1,9 @@
 open Mirage
 
-let client_ipconfig =
-  let nw = Ipaddr.V4.Prefix.of_address_string_exn "192.168.122.101/24" in
-  let gw = Some (Ipaddr.V4.of_string_exn "192.168.122.1") in
-  { network = nw; gateway = gw }
-
 let sv4 =
-  generic_stackv4 ~config:client_ipconfig default_network
+  generic_stackv4 default_network
 
-let main =
-  let packages = [ package ~sublibs:["ethif"; "arpv4"; "ipv4"; "icmpv4"; "tcp"; "udp"] "tcpip"; package "duration" ] in
-  foreign
-    ~packages
-    "Unikernel.Main" (stackv4 @-> time @-> job)
+let main = foreign "Unikernel.Main" (stackv4 @-> time @-> job)
 
 (* let tracing = mprof_trace ~size:2000000 () *)
 
@@ -21,4 +12,3 @@ let () =
   register "iperf_client" [
     main $ sv4 $ default_time
   ]
-
